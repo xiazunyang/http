21b141bd4b92ce9afa6a15403aa0cf2bc20eab37
Tear丶残阳 <x.z.y.seido@outlook.com>
2024-01-05 18:08:01 +0800

    调整Retry机制为：仅重试GET请求和paths中包含以get开头部分的请求。

--- http/build.gradle.kts
+++ http/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "cn.numeron"
-version = "1.0.8"
+version = "1.0.9"
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
--- http/src/main/kotlin/cn/numeron/okhttp/RetryInterceptor.kt
+++ http/src/main/kotlin/cn/numeron/okhttp/RetryInterceptor.kt
@@ -1,6 +1,7 @@
 package cn.numeron.okhttp
 
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 
@@ -10,9 +11,22 @@ class RetryInterceptor(private val retryCount: Int = 2) : Interceptor {
         return try {
             chain.proceed(chain.request())
         } catch (exception: IOException) {
-            retry(0, exception, chain)
+            if (isAllowRetry(chain.request())) {
+                retry(0, exception, chain)
+            } else throw exception
         }
     }
+
+    /** 是否是GET请求 */
+    private fun isAllowRetry(request: Request): Boolean {
+        if (request.method == "GET") {
+            return true
+        }
+        return request.url.pathSegments.any(::isGetUrl)
+    }
+
+    /** 是否是获取数据的url */
+    private fun isGetUrl(segment: String) = segment.startsWith("get")
 
     private fun retry(count: Int, exception: IOException, chain: Interceptor.Chain): Response {
         return if (count < retryCount) {
